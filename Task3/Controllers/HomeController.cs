54cc646b52eb019c843b12acc1fd73b1406e42e4
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-01-30 15:28:38 +0500

    project setup

---
+++ Task3/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task3.Controllers;
+
+[ApiController]
+[Route("/boburboboyev3_gmail_com")]
+public class HomeController : Controller
+{
+    [HttpGet]
+    public ActionResult<string> Index(string x, string y)
+    {
+        if (int.TryParse(x, out int n1) && int.TryParse(y, out int n2))
+        {
+            if (n1 >= 1 && n2 >= 1)
+            {
+                int gcd = GCD(n1, n2);
+
+                return (n1 * n2 / gcd).ToString();
+            }
+        }
+
+        return "NaN";
+    }
+
+    private int GCD(int x, int y)
+    {
+        while (y != 0)
+        {
+            int r = x % y;
+            x = y;
+            y = r;
+        }
+
+        return x;
+    }
+}

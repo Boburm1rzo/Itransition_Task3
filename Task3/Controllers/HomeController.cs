a85923627b63c00c4d5866f892dc066625b1be11
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-01-30 17:16:19 +0500

    Update HomeController.cs

--- Task3/Controllers/HomeController.cs
+++ Task3/Controllers/HomeController.cs
@@ -4,34 +4,23 @@ using System.Numerics;
 namespace Task3.Controllers;
 
 [ApiController]
-[Route("/boburboboyev3_gmail_com")]
-public class HomeController : Controller
+[Route("boburboboyev3_gmail_com")]
+public class HomeController : ControllerBase
 {
     [HttpGet]
-    public ActionResult<string> Index([FromQuery] string x, [FromQuery] string y)
+    public string Index([FromQuery] string x, [FromQuery] string y)
     {
         if (BigInteger.TryParse(x, out var n1) && BigInteger.TryParse(y, out var n2))
         {
             if (n1 >= 1 && n2 >= 1)
             {
-                BigInteger gcd = GCD(n1, n2);
+                BigInteger gcd = BigInteger.GreatestCommonDivisor(n1, n2);
+                BigInteger lcm = (n1 * n2) / gcd;
 
-                return (n1 * n2 / gcd).ToString();
+                return lcm.ToString();
             }
         }
 
         return "NaN";
-    }
-
-    private BigInteger GCD(BigInteger x, BigInteger y)
-    {
-        while (y != 0)
-        {
-            var r = x % y;
-            x = y;
-            y = r;
-        }
-
-        return x;
     }
 }
